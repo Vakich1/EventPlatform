530a16a3bf61cffa3540b79201fe63f2295d221e
Vakich1 <wako7508@gmail.com>
2026-06-12 20:11:03 +0300

    fix: remove CancellationToken from ExecuteSqlRawAsync params to fix type mapping error

--- src/EventPlatform.Infrastructure/Persistence/ApplicationDbContext.cs
+++ src/EventPlatform.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -25,7 +25,6 @@ public class ApplicationDbContext : DbContext, IApplicationDbContext
     {
         return await Database.ExecuteSqlRawAsync(
             "UPDATE \"TicketTypes\" SET \"SoldQuantity\" = \"SoldQuantity\" + 1 WHERE \"Id\" = {0} AND \"SoldQuantity\" < \"TotalQuantity\"",
-            cancellationToken,
-            new object[] { ticketTypeId });
+            ticketTypeId);
     }
 }
